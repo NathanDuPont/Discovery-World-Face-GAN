1256a994ead208aa5a3f8310cbaeb200638879f6
Stuart Harley <harleys@msoe.edu>
2021-03-26 01:30:54 -0500

    modified the face detection scripts to require less arguments for ease of use. Created a simple README explaining how to run them. Moved the images to an test_images folder

--- deep-learning-face-detection/detect_faces_video.py
+++ deep-learning-face-detection/detect_faces_video.py
@@ -12,14 +12,14 @@ import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
+ap.add_argument("-v", "--video", required=True,
+                help="path to input video file")
+ap.add_argument("-p", "--prototxt", default='deploy.prototxt.txt',
                 help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
+ap.add_argument("-m", "--model", default='res10_300x300_ssd_iter_140000.caffemodel',
                 help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
-ap.add_argument("-v", "--video", required=True,
-                help="path to input video file")
 args = vars(ap.parse_args())
 
 # load our serialized model from disk
